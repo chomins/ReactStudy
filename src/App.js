1cdd74a0fc18bb8a94d03e06e4259ac39fa887b3
minsu <cms4317@naver.con>
2021-04-08 16:58:01 +0900

    dd

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        힘내 < code > 학영좌 < /code> 컴퓨터 < /
+        p > <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Learn React <
+        /a> < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
